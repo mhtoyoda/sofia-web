97377a73c3facb1fb3edc98962aa890bb3d79b73
Marcelo Toyoda <marcelohtoy@gmail.com>
2017-01-16 15:28:24 -0200

    Ajuste de login ficticio

--- src/main/java/br/com/webmobidata/web/controller/LoginController.java
+++ src/main/java/br/com/webmobidata/web/controller/LoginController.java
@@ -1,14 +1,27 @@
 package br.com.webmobidata.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import br.com.webmobidata.web.model.Usuario;
 
 @Controller
 public class LoginController {
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String login(){
+	public String login(Model model){
+		model.addAttribute("usuario", new Usuario());
+		return "login";
+	}
+	
+	@RequestMapping(value = "/", method = RequestMethod.POST)
+	public String processLogin(Model model, Usuario usuario){
+		if( null != usuario && usuario.getUsername().equals("teste@gmail.com") && usuario.getPassword().equals("12345")){
+			model.addAttribute("usuario", usuario);
+			return "processo";
+		}
 		return "login";
 	}
 }
